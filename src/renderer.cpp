069a74348d14d236a23f3da54ef59fa1dd6868a7
Neel Raiyani <11281115+Roy-Fokker@users.noreply.github.com>
2022-10-11 21:52:27 -0400

    change how layers and extensions are selected for

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,15 +5,16 @@ using namespace std::string_literals;
 
 namespace 
 {
-	constexpr auto validation_layers = std::array{
+	constexpr auto wanted_layers = std::array{
 		"VK_LAYER_KHRONOS_validation",
+		"VK_LAYER_LUNARG_standard_validation",
 	};
 
-	constexpr auto extension_names = std::array{
+	constexpr auto wanted_extensions = std::array{
 		VK_KHR_SURFACE_EXTENSION_NAME,
 		VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
 		VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
-		"VK_KHR_win32_surface",
+		VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
 	};
 
 	auto get_window_name(HWND handle) -> std::string
@@ -25,6 +26,47 @@ namespace
 		name.resize(len - 1);
 
 		return name;
+	}
+
+	template <size_t S>
+	auto find_best_extensions(const std::array<const char *, S> &wanted, 
+	                          const std::vector<vk::ExtensionProperties> &installed)
+	  -> std::vector<const char *>
+	{
+		auto out = std::vector<const char *>{};
+		for (auto &&w : wanted)
+		{
+			for (auto &&i : installed)
+			{
+				if (std::string(i.extensionName.data()).compare(w) == 0)
+				{
+					out.emplace_back(w);
+					break;
+				}
+			}
+		}
+
+		return out;
+	}
+
+	template <size_t S>
+	auto find_best_layers(const std::array<const char *, S> &wanted, 
+	                      const std::vector<vk::LayerProperties> &installed)
+	  -> std::vector<const char *>
+	{
+		auto out = std::vector<const char *>{};
+		for (auto &&w : wanted)
+		{
+			for (auto &&i : installed)
+			{
+				if (std::string(i.layerName.data()).compare(w) == 0)
+				{
+					out.emplace_back(w);
+					break;
+				}
+			}
+		}
+		return out;
 	}
 }
 
@@ -41,19 +83,25 @@ void renderer::create_vulkan_instance(std::string_view name)
 {
 	try
 	{	
-		auto appInfo = vk::ApplicationInfo(
-			name.data(),
-			VK_MAKE_VERSION(1, 0, 0),
-			name.data(),
-			VK_MAKE_VERSION(1, 0, 0),
-			VK_API_VERSION_1_3
-		);
+		auto appInfo = vk::ApplicationInfo{} = {
+			.pApplicationName = name.data(),
+			.applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+			.pEngineName = name.data(),
+			.engineVersion = VK_MAKE_VERSION(1, 0, 0),
+			.apiVersion = VK_API_VERSION_1_3
+		};
+
+		auto installed_extensions = vk::enumerateInstanceExtensionProperties();
+		auto extensions = find_best_extensions(wanted_extensions, installed_extensions);
+
+		auto installed_layers = vk::enumerateInstanceLayerProperties();
+		auto layers = find_best_layers(wanted_layers, installed_layers);
 
 		auto createInfo = vk::InstanceCreateInfo(
-			{}, 
+			vk::InstanceCreateFlags{}, 
 			&appInfo,
-			validation_layers, 
-			extension_names
+			layers, 
+			extensions
 		);
 
 		instance = vk::createInstance(createInfo);
@@ -69,3 +117,4 @@ void renderer::create_vulkan_instance(std::string_view name)
 		throw err;
 	}
 }
+
